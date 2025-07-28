51cd2da6009bdf60c84dcc3dcd509105f2ae07f0
Sumit Raj <sumitraj1533@gmail.com>
2025-07-29 00:01:22 +0530

    Bug Fix-7

--- router/recruiterRouter.js
+++ router/recruiterRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deleteRequestedJob, getRecruiterJobs,fetchAllRecruiters, requestAJobPosting,updateRequestedJob } from "../controller/recruiterController.js";
+import { deleteRequestedJob, getRecruiterJobs,getAllPartner,getPartnersInCSV, requestAJobPosting,updateRequestedJob } from "../controller/recruiterController.js";
 
 const recruiterRouter = Router();
 
@@ -7,7 +7,7 @@ recruiterRouter.use("/requestJobPosting",requestAJobPosting)
 recruiterRouter.use("/getJobsPosted",getRecruiterJobs);
 recruiterRouter.use("/deleteJob",deleteRequestedJob)
 recruiterRouter.use("/updateJob",updateRequestedJob)
-recruiterRouter.use("/fetchAllRecruiters",fetchAllRecruiters)
+recruiterRouter.use("/fetchAllRecruiters",getAllPartner)
 recruiterRouter.use("/getPartnerInCSV",getPartnersInCSV)
 
 export default recruiterRouter;
