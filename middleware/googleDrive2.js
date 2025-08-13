ba4ad7b374f065e0d0c083fe560dfcac5b9c8e44
admin-srija-consultancy <admin@srijaconsultancy.in>
2025-08-13 16:38:30 +0530

    Update googleDrive2.js

--- middleware/googleDrive2.js
+++ middleware/googleDrive2.js
@@ -9,7 +9,7 @@ const TOKEN_DOC = doc(db, "system", "googleDriveToken");
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI
+   'https://srija-consultancy-backend-llao.onrender.com/oauth2callback'
 );
 
 // Helper: Convert buffer to readable stream
